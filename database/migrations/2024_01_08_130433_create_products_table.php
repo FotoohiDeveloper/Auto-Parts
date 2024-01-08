38907477b3f7851fef2e70277eb5bc04c582d22f
Ali Fotoohi <fotoohideveloper@gmail.com>
2024-01-08 14:59:32 +0330

    Add product database

---
+++ database/migrations/2024_01_08_130433_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('admin_id');
+            $table->string('name');
+            $table->string('price')->default(0);
+            $table->string('color');
+            $table->string('brand');
+            $table->text('description')->collation('utf8_general_ci');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
