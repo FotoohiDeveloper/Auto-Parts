8854d23c5ddece2feb4337a2a29bbfc72e270058
Ali Fotoohi <fotoohideveloper@gmail.com>
2024-01-08 10:33:21 +0330

    Update User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'national_code',
+        'address',
+        'phone_number',
         'email',
         'password',
     ];
